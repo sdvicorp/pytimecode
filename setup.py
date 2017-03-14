02aac3406b0c3295efd5cb84c48df060a90b8792
cmercer <longbowmail@yahoo.com>
2017-03-14 16:53:46 -0700

    let pytimecode be wheelable

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='pytimecode',
       version='1.0',
       description='Python Timecode Utilities',
       author='Joshua Banton',
       author_email='',
-      url='https://github.com/dwelch91/pytimecode/',
+      url='https://github.com/sdvicorp/pytimecode/',
       py_modules=['pytimecode'],
      )
